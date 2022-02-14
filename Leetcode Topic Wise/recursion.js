d6c8778d5700f0b453c6a06885a8322eb9089854
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2022-02-14 22:28:37 +0530

    Update recursion.js

--- Leetcode Topic Wise/recursion.js
+++ Leetcode Topic Wise/recursion.js
@@ -5,7 +5,7 @@
  * Recurrence relation -> Fib(n) = Fib(n-1) + Fib(n-2)
  */
 function fibonacci(n){
-    if(n==1 || n==0){ // base condition - represented by answers we already have
+    if(n<2){ // base condition - represented by answers we already have
         return n;     // in this case we know Fib(0)=0 and Fib(1)=1
     }
     return fibonacci(n-1) + fibonacci(n-2); // add the previous two numbers
