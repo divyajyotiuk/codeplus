94971c7293e82081ad851a630d0b9b7960bcc30c
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2022-02-18 01:08:17 +0530

    Update recursive_sort.js

--- Leetcode Topic Wise/recursive_sort.js
+++ Leetcode Topic Wise/recursive_sort.js
@@ -23,3 +23,33 @@ function recursiveBubbleSort(a,i,j){
 
 // a = recursiveBubbleSort([4,5,1,2,3],4,1);
 // console.log(a);
+
+
+/**
+ * selection sort using recursion
+ */
+
+function recursiveSelectionSort(a,i,j,min){
+
+    console.log("recursiveSelectionSort(",a,i,j,")");
+
+    if(i == a.length-1) return a;
+
+    if(a[j] < a[min]){
+        min = j;
+    }
+
+    if(j < a.length-1){
+        return recursiveSelectionSort(a,i,j+1,min);
+    }
+
+    // swap
+    let temp = a[i];
+    a[i] = a[min];
+    a[min] = temp;
+
+    return recursiveSelectionSort(a,i+1,i+2,i+1);
+}
+
+// a = recursiveSelectionSort([4,5,1,2,3],0,1,0);
+// console.log(a);
