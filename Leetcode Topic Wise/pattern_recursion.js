eeccfc7195d4e12ff6ad12eb7d51d0d91cedc821
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2022-02-18 00:16:35 +0530

    Update pattern_recursion.js

--- Leetcode Topic Wise/pattern_recursion.js
+++ Leetcode Topic Wise/pattern_recursion.js
@@ -8,6 +8,32 @@ Pattern -
 --------------
 */
 
+function patternTriangleInverted(r,c=0){
+
+    console.log("patternTriangleInverted(",r-1,c,")");
+
+    if((r-1) == 0) return "*";
+
+    if(c < (r-1) ){
+        return "* " + patternTriangleInverted(r,c+1);
+    }else{
+        return  "*" + "\r\n" + patternTriangleInverted(r-1,0);
+    }
+}
+
+// a = patternTriangleInverted(4);
+// console.log(a);
+
+/*
+Pattern - 
+--------------
+*
+* * 
+* * *
+* * * *
+--------------
+*/
+
 function patternTriangle(r,c=0){
 
     console.log("patternTriangle(",r-1,c,")");
@@ -15,9 +41,9 @@ function patternTriangle(r,c=0){
     if((r-1) == 0) return "*";
 
     if(c < (r-1) ){
-        return "* " + patternTriangle(r,c+1);
+        return patternTriangle(r,c+1) + " *";
     }else{
-        return  "*" + "\r\n" + patternTriangle(r-1,0);
+        return patternTriangle(r-1,0) + "\r\n" + "*";
     }
 }
 
