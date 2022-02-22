5d3930d1648d27e074d054d4488ce4f914d815d5
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2022-02-23 00:14:29 +0530

    Update subset_subseq.js

--- Leetcode Topic Wise/subset_subseq.js
+++ Leetcode Topic Wise/subset_subseq.js
@@ -20,5 +20,35 @@ function subsequence(processedStr, unprocessedStr, arr=[]){
     return arr;
 }
 
-ans = subsequence("","abc");
-console.log(ans);
+// ans = subsequence("","abc");
+// console.log(ans);
+
+/*
+ * Subsequence with Ascii using recursion
+ */
+function subsequenceWithAscii(processedStr, unprocessedStr, arr=[]){
+    console.log("subsequenceWithAscii( ",processedStr,",",unprocessedStr,",",arr," )");
+
+    if(unprocessedStr.length == 0){
+        return;
+    }
+
+    // include the unprocessedStr[0] in processedStr
+    // move ahead
+    subsequenceWithAscii(processedStr + unprocessedStr[0],unprocessedStr.substring(1),arr);
+    arr.push(processedStr + unprocessedStr[0]);
+
+    // ignore the unprocessedStr[0], send processedStr as is forward
+    subsequenceWithAscii(processedStr,unprocessedStr.substring(1),arr);
+
+    // include the ascii of unprocessedStr[0] in processedStr
+    let withAscii = unprocessedStr.charCodeAt(0);
+    subsequenceWithAscii(processedStr + withAscii, unprocessedStr.substring(1), arr);
+    arr.push(processedStr + withAscii);
+
+    return arr;
+}
+
+// ans = subsequenceWithAscii("","abc");
+// console.log(ans);
+
