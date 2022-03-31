0c1755aa5b2aefc426ac0dfd0e011017071524b7
Divyajyoti Ukirde <30872426+divyajyotiuk@users.noreply.github.com>
2022-03-31 23:32:20 +0530

    Create kthSmallestInBST.js

---
+++ AlgoMonster/DFS/kthSmallestInBST.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(val, left=null, right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+let binTree = new Node(6);
+binTree.left = new Node(4);
+binTree.left.left = new Node(3);
+binTree.left.right = new Node(5);
+binTree.right = new Node(8);
+
+// Leetcode Medium
+// Time complexity O(n)
+function kthSmallest(bst, k) {
+    let num = 0;
+    let kSmall = null;
+    let inorder = function(root, k){
+        if(root == null){
+            return;
+        }
+        
+        inorder(root.left, k);
+        num = num + 1;
+        if(root && k == num){ // we want to end here
+            kSmall = root.val;
+        }
+        inorder(root.right, k);
+        
+    }
+    inorder(bst, k);
+    return kSmall;
+}
+
+let val = kthSmallest(binTree, 2);
+console.log(val);
